cf79a55604cd7f5ebb5136fad395ade5ff8113bf
TheTechWin <88525903+TheTechWin@users.noreply.github.com>
2021-10-11 13:30:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,14 @@ function startLaunchExplorer() {
         start_Action();
     }, 300);
 }
+/*Context menu*/
+window.addEventListener("contextmenu",function(event){
+    event.preventDefault();
+    var contextElement = document.getElementById("context-menu");
+    contextElement.style.top = event.offsetY + "px";
+    contextElement.style.left = event.offsetX + "px";
+    contextElement.classList.add("active");
+  });
+  window.addEventListener("click",function(){
+    document.getElementById("context-menu").classList.remove("active");
+  });
